20ab51d48fdac57a0c00557ff11613fa3fed7fa0
HebeGarcia <heber_gar@hotmail.com>
2021-11-15 14:47:09 -0500

    Tercer_Commit

--- primo.js
+++ primo.js
@@ -2,8 +2,11 @@ let numero = 8
 
 function esPrimo(numero) {
     let cont = 0
-    for (let i == 0; i <= array.length; i++=) {
-        const element = array[i];=
+    for (let i = 0; i <=numero; i++) {
+        if (num) {
+            cont++ 
+        }
+        const element = array[i];
         
     }
     
